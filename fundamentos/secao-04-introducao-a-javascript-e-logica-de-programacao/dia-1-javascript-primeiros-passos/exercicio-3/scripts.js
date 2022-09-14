2d9b9425a8d705c6bbaf66166c501f61e7736cd6
Fernando Frali <112976898+FernandoFrali@users.noreply.github.com>
2022-09-14 17:05:59 -0300

    Adiciona o exercício 3

---
+++ fundamentos/secao-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-3/scripts.js
@@ -0,0 +1,13 @@
+const nota = 100; 
+
+if (nota >= 80) {
+  console.log("Parabéns, você foi aprovado");
+}
+
+else if (nota < 80 && nota >= 60) {
+  console.log("Você está na nossa lista de espera");
+}
+
+else {
+  console.log("Você foi reprovada(o)");
+}
