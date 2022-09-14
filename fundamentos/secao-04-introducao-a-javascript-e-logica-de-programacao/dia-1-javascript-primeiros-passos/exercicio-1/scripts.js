63d70cba293622a46372bf5ec77eefedf1193beb
Fernando Frali <112976898+FernandoFrali@users.noreply.github.com>
2022-09-14 15:19:08 -0300

    Adiciona o exercício 1

---
+++ fundamentos/secao-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-1/scripts.js
@@ -0,0 +1,13 @@
+const myName = "Fernando";
+const birthCity = "Osasco";
+const birthYear = 2001;
+
+birthYear = 2030;
+birthCity = "São Sebastião";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+// Recebemos a mensagem de erro pois não podemos alterar constantes. Nesse caso, seria melhor o uso da variável "let"
+
