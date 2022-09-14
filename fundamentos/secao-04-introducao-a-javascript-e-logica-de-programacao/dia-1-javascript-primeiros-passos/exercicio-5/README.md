6d1d3f137bc5975a884d0f1afce3b444e2c2d00d
Fernando Frali <112976898+FernandoFrali@users.noreply.github.com>
2022-09-14 18:56:18 -0300

    Adiciona o exercício 5 de JavaScript Básico

---
+++ fundamentos/secao-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-5/README.md
@@ -0,0 +1,3 @@
+# Exercício 5
+
+Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
---
+++ fundamentos/secao-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-5/scripts.js
@@ -0,0 +1,16 @@
+let anguloUm = 80;
+let anguloDois = 50;
+let anguloTres = 50;
+let resultado = anguloUm + anguloDois + anguloTres;
+
+if (resultado === 180 && resultado > 0) {
+  console.log(true);
+}
+
+else if (resultado !== 180 && resultado > 0) { 
+  console.log(false);
+}
+
+else {
+  console.log("error");
+}
