5895a771c83c4d89ea423ec4fe0a207b982b4b30
Fernando Frali <112976898+FernandoFrali@users.noreply.github.com>
2022-10-28 23:08:11 -0300

    Adiciona o dia 4 e 5

---
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/script.js
@@ -0,0 +1,19 @@
+const objcts = {
+  carro: "Gol",
+  ano: 2001,
+  cor: "cinza",
+};
+
+const objects2 = {
+  tamanho: "metros",
+  rodas: "calibradas",
+  cores: {
+    cor1: "black",
+    cor2: "pink",
+    cor3: "cinza",
+  },
+};
+const arrayz = ["Um", "Dois", "Três", "Quatro"];
+const [, , one, two, three, four] = arrayz;
+
+console.log(one);
